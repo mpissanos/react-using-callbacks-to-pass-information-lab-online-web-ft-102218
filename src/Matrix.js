e9870b23f1e2775f097335e17e18ed8ca37d645c
Manny Pissanos <mpissano@gmail.com>
2019-04-01 21:56:48 +0000

    Automatically backed up by Learn

--- src/Matrix.js
+++ src/Matrix.js
@@ -17,8 +17,7 @@ export default class Matrix extends Component {
        selectedColor: newColor
      })
    }
-    
-  }
+
 
   genRow = (vals) => (
     vals.map((val, idx) => <Cell key={idx} color={val} />)
